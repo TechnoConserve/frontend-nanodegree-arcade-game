7f1206b7bdf20d0294e218927d09bb0d7072321b
Avery Uslaner <uslaner.avery@gmail.com>
2018-06-12 10:41:21 -0600

    Coordinate handling for entities
    Added coordinate properties for player and enemy objects as well as
    keyboard input handling to update the coordinates of objects on the game
    canvas.
    
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-const Enemy = function() {
+const Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -24,8 +26,10 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Character = function () {
-    this.sprite = 'images/char-boy.png'
+const Character = function (x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
 };
 
 Character.prototype.update = function (dt) {
@@ -36,19 +40,35 @@ Character.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Update the character coordinates based on keyboard input
+// Checks are made to ensure coordinates are not updated
+// beyond the game canvas
 Character.prototype.handleInput = function (keyPress) {
-
+    if (keyPress === "left" && this.x > 0) {
+        this.x -= 100;
+    } else if (keyPress === "up" && this.y > -25) {
+        this.y -= 81;
+    } else if (keyPress === "right" && this.x < 400) {
+        this.x += 100;
+    } else if (keyPress === "down" && this.y < 380) {
+        this.y += 81;
+    }
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// allEnemies stores our enemy objects
 let allEnemies = [];
+// loop to create multiple enemies
 for (let i = 0; i < 5; i++) {
-    const enemy = new Enemy();
+    // We want the enemies to spawn on one of the three
+    // stone rows
+    const block = getRandomStoneBlock();
+    const enemy = new Enemy(block.x, block.y);
     allEnemies.push(enemy);
 }
-const player = new Character();
+
+// Create a player object that will be located on the bottom
+// row, middle column
+const player = new Character(200, 380);
 
 
 // This listens for key presses and sends the keys to your
@@ -63,3 +83,14 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+// Return coordinates to one of the stone blocks
+// Coordinate range is from (0, 56) to (400, 218)
+function getRandomStoneBlock() {
+    // Get a random row but add one since we don't want
+    // a water block which would be row 0
+    const row = Math.floor(Math.random() * Math.floor(3)) + 1;
+    const col = Math.floor(Math.random() * Math.floor(5));
+    return {x: col * 100, y: row * 81 - 25};
+}
